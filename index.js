5197f3cef2d50e80adfd0a9a9fd18dcf791e0ed5
Zoraarchiee <chairulapriliansyah27@gmail.com>
2023-07-10 17:44:00 +0700

    (~) Popub boxXy

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 var x = 30;
 console.log("hello world!");
 console.log("Isi dari variabel x adalah " + x);
-console.log('Chairul');
+console.log("Chairul");
+
+// Popup box: alert;
+alert("Hello!");
+
+
+// Popup box: prompt;
+var name = prompt("Enter Your name!");
+alert(name);
+console.log(name);
+
+// Popup box: confirm;
+var popUp = confirm('Yakin dek?');
+alert(popUp);
