5223a0066aefae427ea2e34606dd2951f38f3079
Zoraarchiee <chairulapriliansyah27@gmail.com>
2023-07-14 17:27:56 +0700

    (~) Pengulangan dan Pengkondisian.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-// alert("Welcome to my site!");
-// var again = true;
+alert("Welcome to my site!");
+var again = true;
 
-// while (again === true) {
-//   var name = prompt("Enter your name!");
-//   alert("Halo!, " + name)
+while (again === true) {
+  var name = prompt("Enter your name!");
+  alert("Halo!, " + name)
 
-//   again = confirm("You want try again?");
-// }
+  again = confirm("You want try again?");
+}
 
-// alert("Thank You!!");
+alert("Thank You!!");
 
-let x = 30;
-let y = 20;
-let z = x + y;
-console.log(z);
+
+alert('Start');
+for(var a = 0; a < 5; a++) {
+    alert('hi!');
+}
+alert('finish');
+
+
+var number = prompt('enter your number : ');
+if( number % 2 === 0 ) {
+    alert(number + ' even');
+} else {
+    alert(number + ' odd');
+}
