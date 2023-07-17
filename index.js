6ac8c3e45fa454a222f63c046d213c1ad661205a
Zoraarchiee <chairulapriliansyah27@gmail.com>
2023-07-17 15:01:43 +0700

    (~) JavaScript repetition

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+var angkot = 1;
+while (angkot <= 10) {
+    console.log(`Angkot No. ${angkot} beroperasi dengan baik.`);
+    angkot++;
+}
+
+var motor = 1;
+while (motor <= 10) {
+    console.log('Motor No.' + motor + ' berjalan dengan baik');
+    motor++;
+}
+
 alert("Welcome to my site!");
 var again = true;
 
 while (again === true) {
   var name = prompt("Enter your name!");
-  alert("Halo!, " + name)
+  console.log(name);
+  alert("Halo!, " + name);
 
   again = confirm("You want try again?");
 }
@@ -21,6 +34,8 @@ alert('finish');
 var number = prompt('enter your number : ');
 if( number % 2 === 0 ) {
     alert(number + ' even');
+    console.log(number + ' even');
 } else {
     alert(number + ' odd');
+    console.log(`${number} odd`);
 }
