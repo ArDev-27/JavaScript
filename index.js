c829747226b67d2e625f28a96cf9d50f2181281d
Zoraarchiee <chairulapriliansyah27@gmail.com>
2023-07-18 17:41:45 +0700

    (+) NAVBAR

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var angkot = 1;
-while (angkot <= 10) {
-    console.log(`Angkot No. ${angkot} beroperasi dengan baik.`);
-    angkot++;
+var noAngkot = 1;
+var jmlAngkot = 10;
+while (noAngkot <= jmlAngkot) {
+  console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
+  noAngkot++;
 }
 
 var motor = 1;
 while (motor <= 10) {
-    console.log('Motor No.' + motor + ' berjalan dengan baik');
-    motor++;
+  console.log("Motor No." + motor + " berjalan dengan baik");
+  motor++;
+}
+
+for(var human = 1; human <= 10; human++) {
+    console.log(`${human} Human`);
 }
 
 alert("Welcome to my site!");
-var again = true;
 
+var again = true;
 while (again === true) {
   var name = prompt("Enter your name!");
   console.log(name);
@@ -23,19 +28,17 @@ while (again === true) {
 
 alert("Thank You!!");
 
-
-alert('Start');
-for(var a = 0; a < 5; a++) {
-    alert('hi!');
+alert("Start");
+for (var a = 0; a < 5; a++) {
+  alert("hi!");
 }
-alert('finish');
+alert("finish");
 
-
-var number = prompt('enter your number : ');
-if( number % 2 === 0 ) {
-    alert(number + ' even');
-    console.log(number + ' even');
+var number = prompt("enter your number : ");
+if (number % 2 === 0) {
+  alert(number + " even");
+  console.log(number + " even");
 } else {
-    alert(number + ' odd');
-    console.log(`${number} odd`);
+  alert(number + " odd");
+  console.log(`${number} odd`);
 }
