e382607787981bee01a95f34fb3b9788bd321f9e
Zoraarchiee <chairulapriliansyah27@gmail.com>
2023-07-18 23:22:09 +0700

    (+) New page

---
+++ angkot2.js
@@ -0,0 +1,35 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+while (noAngkot <= angkotBeroperasi) {
+  console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
+  noAngkot++;
+}
+
+for (noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
+  console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi.`);
+}
+
+var jmlOjek = 10;
+var ojekOperation = 5;
+for (var noOjek = 1; noOjek <= ojekOperation; noOjek++) {
+  console.log("Ojek No. " + noOjek + " sedang ngater pelanggan.");
+}
+for (noOjek = ojekOperation + 1; noOjek <= jmlOjek; noOjek++) {
+  console.log("Ojek No. " + noOjek + " sedang magkal nungguin pelanggan.");
+}
+
+
+// error 
+// var jmlGrab = 10;
+// var grabBeroperasi = 6;
+// var noGrab = 1;
+// while (noGrab <= grabBeroperasi) {
+//   console.log(`Grab No. ${noGrab} sedang beroperasi.`);
+//   noGrab++;
+// }
+
+// while (grabBeroperasi <= jmlGrab) {
+//   console.log("Grab No. " + grabBeroperasi + " sedang tidak beroperasi.");
+//   grabBeroperasi++;
+// }
